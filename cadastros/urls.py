77a1723a341edb71fef61dc37360c8035ef92c7e
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-10-16 11:44:53 -0300

    add: dinamic links

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -4,5 +4,5 @@ from cadastros.views import lista_cidades, detalhe_cidade
 
 urlpatterns = [
     path('', lista_cidades, name='cidades-list'),
-    path('detalhe/<int:id>', detalhe_cidade, name='cidades-detalhe'),
+    path('detalhe/<int:id>/', detalhe_cidade, name='cidades-detalhe'),
 ]
