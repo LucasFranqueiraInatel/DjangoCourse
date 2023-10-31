0238d17b8bf407c5dffac7004a7551bdf67b1388
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-10-31 10:56:13 -0300

    add: logout

--- proj/urls.py
+++ proj/urls.py
@@ -23,5 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cadastros.urls')), #http://proj/cidades
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'))
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+
 ]
