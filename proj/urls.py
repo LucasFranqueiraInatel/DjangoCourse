b8c3ce79c050fa31a628b889ac017169451a1cb4
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-10-31 11:46:02 -0300

    update: authentication system

--- proj/urls.py
+++ proj/urls.py
@@ -22,8 +22,12 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cadastros.urls')), #http://proj/cidades
+    path('', include('cadastros.urls')),  # http://proj/cidades
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
-
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset-password-done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset-password-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
