a4b1f1c3a96af13649f11ff920ac48270076553f
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-03 11:29:44 -0300

    update: tickets

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -4,6 +4,16 @@ from cadastros.models import Cidade, Estado, Pais
 
 # Register your models here.
 
+class EstadoInline(admin.TabularInline):
+    model = Estado
+
+class PaisAdmin(admin.ModelAdmin):
+
+    fields = ('nome', )
+    inlines = [
+        EstadoInline
+    ]
+
 admin.site.register(Cidade)
 admin.site.register(Estado)
-admin.site.register(Pais)
+admin.site.register(Pais, PaisAdmin)
--- proj/settings.py
+++ proj/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'cadastros'
+    'cadastros',
+    'tickets'
 ]
 
 MIDDLEWARE = [
@@ -128,12 +129,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
-MEDIA_ROOT = 'media'
-MEDIA_URL = 'files'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static_files",
 ]
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = 'files/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
