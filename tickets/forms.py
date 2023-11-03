9881e7854a72052bbe0cfbe84e75b944a24f2b1b
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-03 14:20:26 -0300

    update: forms.py

---
+++ tickets/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from tickets.models import Solicitacao
+
+
+class NovaSolicitacaoForm(forms.ModelForm):
+
+    class Meta:
+        model = Solicitacao
+        fields = ['categoria', 'nome', 'email', 'assunto', 'descricao', 'arquivo']
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.layout = Layout(
+    #         Fieldset(
+    #             'Dados pessoais',
+    #             'nome',
+    #             'email'
+    #         ),
+    #         Fieldset(
+    #             'Mensagem',
+    #             'categoria',
+    #             'assunto',
+    #             'descricao',
+    #             'arquivo'
+    #         ),
+    #         ButtonHolder(
+    #             Submit('submit', 'Enviar solicitação', css_class='btn btn-primary')
+    #         )
+    #     )
