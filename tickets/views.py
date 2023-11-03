0ab38621fd5c7a4bb88edec9c812a597f70028b8
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-03 15:50:21 -0300

    update: Tickets forms

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from tickets.forms import NovaSolicitacaoForm
+from tickets.models import Interacao
 
 
 class NovaSolicitacao(View):
@@ -12,18 +13,18 @@ class NovaSolicitacao(View):
 
         return render(request, 'tickets/form.html', {'form': form})
 
-    # def post(self, request):
-    #
-    #     form = NovaSolicitacaoForm(request.POST, request.FILES)
-    #
-    #     if form.is_valid():
-    #
-    #         obj = form.save(commit=True)
-    #         obj.save()
-    #
-    #         interacao = Interacao.objects.create(solicitacao=obj, tipo=Interacao.TIPO_STATUS_CHANGE, descricao='Solicitação aberta')
-    #         interacao.send_mail_message()
-    #
-    #         return redirect('cidades-list')
-    #
-    #     return render(request, 'tickets/form.html', {'form': form})
+    def post(self, request):
+
+        form = NovaSolicitacaoForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            obj = form.save(commit=True)
+            obj.save()
+
+            # interacao = Interacao.objects.create(solicitacao=obj, tipo=Interacao.TIPO_STATUS_CHANGE, descricao='Solicitação aberta')
+            # interacao.send_mail_message()
+
+            return redirect('cidades-list')
+
+        return render(request, 'tickets/form.html', {'form': form})
