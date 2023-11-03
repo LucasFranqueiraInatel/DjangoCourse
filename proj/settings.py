411f21dc5869484cbe0fa180e2f2219023241674
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-03 14:38:05 -0300

    update: Tickets forms

--- proj/settings.py
+++ proj/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'crispy_forms',
     'cadastros',
     'tickets'
 ]
@@ -136,6 +137,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = 'files/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
