9d798a4b6c76564e07b9f257ac5a9081a3495714
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-03 14:20:40 -0300

    update: views.py

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from tickets.forms import NovaSolicitacaoForm
+
+
+class NovaSolicitacao(View):
+
+    def get(self, request):
+
+        form = NovaSolicitacaoForm
+
+        return render(request, 'tickets/form.html', {'form': form})
+
+    # def post(self, request):
+    #
+    #     form = NovaSolicitacaoForm(request.POST, request.FILES)
+    #
+    #     if form.is_valid():
+    #
+    #         obj = form.save(commit=True)
+    #         obj.save()
+    #
+    #         interacao = Interacao.objects.create(solicitacao=obj, tipo=Interacao.TIPO_STATUS_CHANGE, descricao='Solicitação aberta')
+    #         interacao.send_mail_message()
+    #
+    #         return redirect('cidades-list')
+    #
+    #     return render(request, 'tickets/form.html', {'form': form})
