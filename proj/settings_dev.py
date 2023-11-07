5e152d88a8fe7a71c63d4f039dc62b7479d06c1b
Lucas Franqueira <lucas.f@gec.inatel.br>
2023-11-07 13:14:59 -0300

    update: database

--- proj/settings_dev.py
+++ proj/settings_dev.py
@@ -4,29 +4,29 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 INTERNAL_IPS = ('127.0.0.1', 'localhost',)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sidia',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5433',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sidia',
-        'CLIENT': {
-                'host': '127.0.0.1',
-                'port': 27016,
-                'username': 'root',
-                'password': 'sidia',
-            },
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'sidia',
+#         'CLIENT': {
+#                 'host': '127.0.0.1',
+#                 'port': 27016,
+#                 'username': 'root',
+#                 'password': 'sidia',
+#             },
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = []
